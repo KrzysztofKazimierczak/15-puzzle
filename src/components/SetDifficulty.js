c2b1501dbaa3730ed64980b297c01a4a46dffea6
Krzysztof Kazimierczak <krz.kazimierczak@gmail.com>
2019-08-31 14:50:12 +0200

    add boardSize to state

--- src/components/SetDifficulty.js
+++ src/components/SetDifficulty.js
@@ -3,21 +3,25 @@ import '../styles/SetDifficulty.css'
 
 
 class SetDifficulty extends Component {
+
+  handleClick = (boardsize) => {
+    this.props.setBoardSize(boardsize)
+  }
   render() {
 
     return (
       <>
         <h2>Select difficulty:</h2>
         <section className="settings">
-          <div>
+          <div onClick={() => { this.handleClick(9) }} >
             <p>easy</p>
             <span>3x3</span>
           </div>
-          <div>
+          <div onClick={() => { this.handleClick(16) }}>
             <p>normal</p>
             <span>4x4</span>
           </div>
-          <div>
+          <div onClick={() => { this.handleClick(25) }}>
             <p>hard</p>
             <span>5x5</span>
           </div>
