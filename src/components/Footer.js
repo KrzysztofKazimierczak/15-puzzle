e4bbd6d472697c4966af27e44dd2c0185b1e77cb
Krzysztof Kazimierczak <krz.kazimierczak@gmail.com>
2019-09-10 11:50:12 +0200

    add heart and author

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import heart from '../images/heart.png';
+import '../styles/Footer.css'
 const Footer = () => {
   return (
-    <p>created with heart</p>
+    <p>created with <img src={heart} alt="heart" /> by <a href="https://github.com/KrzysztofKazimierczak" rel="noopener noreferrer" target="_blank">Krzysztof Kazimierczak</a></p>
   )
 }
 
