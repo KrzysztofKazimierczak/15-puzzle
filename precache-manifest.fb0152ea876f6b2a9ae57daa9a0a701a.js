5dc705626a497904cd00e7ce0cb98976d498947d
Krzysztof Kazimierczak <krz.kazimierczak@gmail.com>
2019-09-10 20:01:18 +0200

    Updates

---
+++ precache-manifest.fb0152ea876f6b2a9ae57daa9a0a701a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79067ea346f10c3281a927c5c0db84f",
+    "url": "/15-puzzle/static/media/bg.e79067ea.jpg"
+  },
+  {
+    "revision": "bae4684018ccced23f88",
+    "url": "/15-puzzle/static/js/runtime~main.8815c511.js"
+  },
+  {
+    "revision": "e9a5e302343a5b16038a",
+    "url": "/15-puzzle/static/js/main.74cb8015.chunk.js"
+  },
+  {
+    "revision": "2be1e72a8436f2ddad1b",
+    "url": "/15-puzzle/static/js/2.163c59ff.chunk.js"
+  },
+  {
+    "revision": "e9a5e302343a5b16038a",
+    "url": "/15-puzzle/static/css/main.abb3c0bb.chunk.css"
+  },
+  {
+    "revision": "fc3709712559514365aa3c55effc1758",
+    "url": "/15-puzzle/index.html"
+  }
+];
